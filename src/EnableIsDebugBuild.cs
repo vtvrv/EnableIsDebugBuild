76997611bbaba24cd6de1b0cb4f384fbe1159125
vtvrv <98350357+vtvrv@users.noreply.github.com>
2022-01-24 20:55:29 +0000

    Initial Upload

---
+++ src/EnableIsDebugBuild.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+
+namespace BehaviourInjector
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class FreeMovePlugin
+
+#if MONO
+        : BaseUnityPlugin
+#else
+        : BepInEx.IL2CPP.BasePlugin
+#endif
+    {
+        public const string GUID = "com.vtvrvxiv.EnableIsDebugBuild";
+        public const string NAME = "EnableIsDebugBuild";
+        public const string AUTHOR = "vtvrvxiv";
+        public const string VERSION = "1.0.0";
+
+#if MONO
+        internal void Awake()
+        {
+            Setup();
+        }
+#else
+        public override void Load()
+        {
+            Setup();
+        }
+#endif
+
+        private void Setup()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks));
+            Debug.Log("Debug.isDebugBuild == " + Debug.isDebugBuild.ToString());
+            //Debug.Log("Debug.developerConsoleVisible == " + Debug.developerConsoleVisible.ToString());
+        }
+    }
+
+
+    internal static class Hooks
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(UnityEngine.Debug), nameof(UnityEngine.Debug.isDebugBuild), MethodType.Getter)]
+        private static void Patch_isDebugBuild(ref bool __result) => __result = true;
+
+
+        //[HarmonyPostfix, HarmonyPatch(typeof(UnityEngine.Debug), nameof(UnityEngine.Debug.developerConsoleVisible), MethodType.Getter)]
+        //private static void Patch_developerConsoleVisible(ref bool __result) => __result = true;
+    }
+}
